ae2add3322387158be92016fcbc872d4e827c270
MikeRubio <miguel.rubiojimenez@gmail.com>
2024-01-01 23:27:17 +0100

    feat: helper component

---
+++ src/app/components/jobs/jobs-card/jobs-card.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input } from '@angular/core';
+import { NewsItem } from '../../../app.model';
+
+@Component({
+  selector: 'app-jobs-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './jobs-card.component.html',
+})
+export class JobsCardComponent {
+  @Input()
+  jobItem!: NewsItem;
+
+  constructor() {}
+
+  getRelativeTime(timestamp: number): string {
+    const date = new Date(timestamp * 1000);
+    const now = new Date();
+    const seconds = Math.floor(
+      (now.getTime() - new Date(date).getTime()) / 1000
+    );
+
+    if (seconds < 60) {
+      return 'Just now';
+    } else if (seconds < 3600) {
+      return Math.floor(seconds / 60) + ' minutes ago';
+    } else if (seconds < 86400) {
+      return Math.floor(seconds / 3600) + ' hours ago';
+    } else {
+      return Math.floor(seconds / 86400) + ' days ago';
+    }
+  }
+}
