83b38b21914ffa9cf0d34eecea8414cd204462ea
MikeRubio <miguel.rubiojimenez@gmail.com>
2024-01-02 01:05:47 +0100

    feat: initial github actions

---
+++ karma.conf.js
@@ -0,0 +1,38 @@
+module.exports = function (config) {
+  config.set({
+    basePath: "",
+    frameworks: ["jasmine", "@angular-devkit/build-angular"],
+    plugins: [
+      require("karma-jasmine"),
+      require("karma-chrome-launcher"),
+      require("karma-jasmine-html-reporter"),
+      require("karma-coverage"),
+      require("@angular-devkit/build-angular/plugins/karma"),
+    ],
+    client: {
+      clearContext: false,
+      jasmine: {
+        timeoutInterval: 10000,
+      },
+    },
+    coverageReporter: {
+      dir: require("path").join(__dirname, "./coverage/your-angular-app"),
+      subdir: ".",
+      reporters: [{ type: "html" }, { type: "text-summary" }],
+    },
+    reporters: ["progress", "kjhtml"],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ["Chrome", "ChromeHeadless", "ChromeHeadlessCI"],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: "ChromeHeadless",
+        flags: ["--no-sandbox", "--disable-gpu"],
+      },
+    },
+    singleRun: false,
+    restartOnFileChange: true,
+  });
+};
