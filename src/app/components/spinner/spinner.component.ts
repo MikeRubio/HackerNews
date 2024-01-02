7837746f2060a71ecdb93ac4c765a9ed25b48f1d
MikeRubio <23417202+MikeRubio@users.noreply.github.com>
2024-01-02 12:11:42 +0000

    Deploying to gh-pages from @ MikeRubio/HackerNews@404d9bbaf639549d0e002384c382c49e876fd4e0 🚀

--- src/app/components/spinner/spinner.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-spinner',
-  standalone: true,
-  imports: [],
-  templateUrl: './spinner.component.html',
-})
-export class SpinnerComponent {}
